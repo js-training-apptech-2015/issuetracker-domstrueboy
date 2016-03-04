d7acbf2fd400affe69f9c761fff7cc4b11e1e9e9
Pavel Bobylyev <domstrueboy@yandex.ru>
2016-03-05 02:51:35 +0500

    Try 3

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -173,7 +173,7 @@ gulp.task('wiredep', () => {
     .pipe(gulp.dest('app'));
 });
 
-gulp.task('build', ['lint', 'html', 'images', 'fonts', 'mustache', 'extras'], () => {
+gulp.task('build', [/*'lint', */'html', 'images', 'fonts', 'mustache', 'extras'], () => {
   return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
